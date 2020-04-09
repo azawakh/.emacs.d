77ce82a74009b05c5ea468573390e245cace08b5
hagevvashi <hagevvashi@gmail.com>
2020-04-09 17:27:47 +0900

    chore: update packages

--- elpa/slim-mode-1.1/slim-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "slim-mode" "1.1" "Major mode for editing Slim files" 'nil :commit "fe8abb644b7b9cc0ed1e76d9ca8d6c01edccbdb8" :keywords '("markup" "language") :authors '(("Nathan Weizenbaum")) :maintainer '("Nathan Weizenbaum") :url "http://github.com/minad/emacs-slim")
